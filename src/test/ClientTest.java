76a87025655ae5edf581150c2f0e6f7151b18e1c
salmakl <salma.kalkhi@gmail.com>
2021-12-15 11:01:23 +0100

    .

---
+++ src/test/ClientTest.java
@@ -0,0 +1,32 @@
+import com.example.brief3.DAO.Client;
+import com.example.brief3.models.Clients;
+import com.example.brief3.DAO.Client;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+
+
+
+class ClientTest {
+
+    @Test
+    public void addTest(){
+
+        /*Clients client = new Clients(345,"123456RF","salma","kalkhi","+212123456789","salma.kalkhi@gmail.com","safi",new Date(2021-12-12),"youcode");
+        Client client1 = new Client();
+        client1.save(client);*
+        /
+         */
+
+        int a=3,b=2;
+        int c=a+b;
+        Assertions.assertEquals(5,c);
+        /*Assertions.assertFalse(client1.getClients().isEmpty());
+        Assertions.assertEquals(1,client1.getClients().size());*/
+
+
+    }
+
+}
